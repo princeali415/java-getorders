6df4e309807f8ceba25ddc36cf60fa5f5dee8d1e
Hussain Ali <ali.415code@gmail.com>
2021-02-16 14:36:21 -0800

    first commit

---
+++ src/main/java/com/lambdaschool/orders/repositories/CustomersRepository.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.orders.repositories;
+
+import com.lambdaschool.orders.models.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomersRepository extends CrudRepository<Customer, Long>
+{
+
+}
